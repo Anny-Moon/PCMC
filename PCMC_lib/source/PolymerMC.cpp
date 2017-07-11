6f9736a05d59cb754c354952de15f5ae2f7a9c33
Anna Sinelnikova <anna.sinelnikova@physics.uu.se>
2017-07-11 14:20:13 +0200

    bug fix in updateR02chans

--- PCMC_lib/source/PolymerMC.cpp
+++ PCMC_lib/source/PolymerMC.cpp
@@ -750,6 +750,9 @@ void PolymerMC::updateR02chains(double temperature, const Interaction& interacti
     GaussRand gRandZ(r[0].z, 0.2);
     r[0].z = gRandZ();
 
+    for(i = 1; i<numMonomers; i++)
+	r[i] = r[i] + r[0];
+    
     /* calculate new interaction for site */
     interactionNew = interaction.energyIfSiteChanged(numMonomers2+1, N12, r12);
 //    printf("oldInt = %g    newInt = %g\n", interactionOld, interactionNew);
