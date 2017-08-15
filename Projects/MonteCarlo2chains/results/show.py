345f721bd7a0f8306daaac45d2c95b699bf6e5e4
Anna Sinelnikova <anna.sinelnikova@physics.uu.se>
2017-08-15 20:39:46 +0200

    improving plotter py

--- Projects/MonteCarlo2chains/results/show.py
+++ Projects/MonteCarlo2chains/results/show.py
@@ -50,17 +50,21 @@ class ChainFile():
 	self.numBlocks = blockNum-1;
 	#---------------------------end--------------------------------
 	print('Number of chains: %i.' % self.numBlocks);
-    def plot(self, ax, confNum):
+    def plot(self, confNum, ax, axMaxRange):
+	r = lambda: random.randint(0,255);
 	r = lambda: random.randint(0,255);
 	colorLine = '#%02X%02X%02X' % (r(),r(),r());
+	sizeLine = 40/axMaxRange;
+	r = lambda: random.randint(0,255);
 	r = lambda: random.randint(0,255);
 	colorDot = '#%02X%02X%02X' % (r(),r(),r());
+	sizeDot = 800/axMaxRange;
 	for i in range (0, self.N[confNum]-1):
 	    line = [(self.chain[confNum].x[i],self.chain[confNum].y[i],self.chain[confNum].z[i]),
 		(self.chain[confNum].x[i+1],self.chain[confNum].y[i+1],self.chain[confNum].z[i+1])];
 	    (x, y, z) = zip(*line);
-	    ax.scatter(x,y,z, c = colorDot, alpha = 1, s=30);
-	    ax.plot(x,y,z, c = colorLine, alpha = 1);
+	    ax.scatter(x,y,z, c = colorDot, alpha = 1, s=sizeDot);
+	    ax.plot(x,y,z, c = colorLine, alpha = 1, lw=sizeLine);
 	
     def plotWithStandardColors(self, ax, confNum):
 	ax.plot(self.chain[confNum].x, self.chain[confNum].y, self.chain[confNum].z, c='#006699');
@@ -80,6 +84,13 @@ class EqualAxes():
 	self.X.append(X);
 	self.Y.append(Y);
 	self.Z.append(Z);
+    def findMaxRange(self):
+	X=np.asarray(self.X);
+	Y=np.asarray(self.Y);
+	Z=np.asarray(self.Z);
+	maxRange = np.array([X.max()-X.min(), Y.max()-Y.min(), Z.max()-Z.min()]).max();
+	return maxRange;
+	
     def set(self,ax):
 	X=np.asarray(self.X);
 	Y=np.asarray(self.Y);
@@ -91,7 +102,6 @@ class EqualAxes():
 	ax.set_xlim(mid_x - max_range, mid_x + max_range);
 	ax.set_ylim(mid_y - max_range, mid_y + max_range);
 	ax.set_zlim(mid_z - max_range, mid_z + max_range);
-
 
 fileName = "Configurations/0confR.dat";
 chain = ChainFile(fileName);
@@ -110,8 +120,14 @@ else:
     for i in range(1,len(sys.argv)):
 	confNum = int(sys.argv[i]);
 	print('Chain %s has %i atoms.' % (sys.argv[i], chain.getN(confNum)));
-	chain.plot(ax,confNum);
 	eqAx.push(chain.getChain(confNum).getX(),chain.getChain(confNum).getY(),chain.getChain(confNum).getZ());
-
+    
+    axMaxRange=eqAx.findMaxRange();
+    
+    for i in range(1,len(sys.argv)):
+	confNum = int(sys.argv[i]);
+	
+	chain.plot(confNum,ax,axMaxRange);
+    
 eqAx.set(ax);
 plt.show();
