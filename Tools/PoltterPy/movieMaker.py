5c1b22887e6304c9ba1661c02a0d959887f0154f
Anna Sinelnikova <anna.sinelnikova@physics.uu.se>
2017-08-18 17:35:17 +0200

    saving fixed on plotter py movieMaker

--- Tools/PoltterPy/movieMaker.py
+++ Tools/PoltterPy/movieMaker.py
@@ -1,5 +1,6 @@
 """
-
+python ./movieMaker.py <path/fileIn.ext> <increment> <gif/mp4> <path/fileOut>
+python ./movieMaker.py 5dn7.pca 3 gif movie
 
 For saving option you need additional packages which
 are not included in 'matplotlib'.
@@ -32,11 +33,8 @@ def init():
 if(len(sys.argv)<2):
     print("Error: Give me name of the file you want me to open.");
     print("Hint: You can also pass me number(s) of configuration(s) to plot.\n");
-fileName = sys.argv[1];
-polymer = Polymer.Polymer(fileName);
-
-#Writer = animation.writers['ffmpeg']
-#writer = Writer(fps=15, metadata=dict(artist='Me'), bitrate=1800)
+fileNameIn = sys.argv[1];
+polymer = Polymer.Polymer(fileNameIn);
 
 fig = plt.figure()
 ax = fig.gca(projection='3d');
@@ -46,17 +44,39 @@ eqAx = EqualAxes.EqualAxes();
 #first frame
 eqAx.push(polymer.getX(0),polymer.getY(0),polymer.getZ(0));
 eqAx.set(ax);
-#polymer.plot(0,ax,100);
 
-increment = 5;
-#eqAx.set(ax);
+increment = 1;
+
+#if pass increment
+if(len(sys.argv)>2):
+    increment = int(sys.argv[2]);
+    if(increment < 1):
+	increment = 1;
+
 anim = animation.FuncAnimation(fig, update,
-		    frames=10,
+		    frames=3,
 		    interval=200,
 		    fargs = (increment,),
 		    repeat = False
 		    );
-#anim.save('movie.gif', dpi=80, writer='imagemagick');
-mywriter = animation.FFMpegWriter(fps=60)
-anim.save('movie.mp4', writer=mywriter);
-plt.show();
+
+#if no saving
+if(len(sys.argv)<4):
+    plt.show();
+
+#if save
+else:
+    extention = sys.argv[3];
+    fileName=fileNameIn[:-3];
+    fileNameOut=fileName + extention;
+    
+    #if pass fileNameOut
+    if(len(sys.argv)>4):
+	fileNameOut = sys.argv[4] + extention;
+    
+    if(sys.argv[3]=='gif'):
+	anim.save('movie.gif', dpi=80, writer='imagemagick');
+    if(sys.argv[3]=='mp4'):
+	writer = animation.FFMpegWriter(fps=2);
+	anim.save('movie.mp4', writer=writer);
+    print("File %s was successfully generated!" % fileNameOut);
