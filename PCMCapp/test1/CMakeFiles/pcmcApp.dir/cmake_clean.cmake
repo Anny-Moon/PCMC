8b5c83712d7d9fd19d7af1d15d96ff4ea2d09a34
Anna Sinelnikova <anna.sinelnikova@physics.uu.se>
2018-04-12 18:53:16 +0200

    added project with cmake

---
+++ PCMCapp/test1/CMakeFiles/pcmcApp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pcmcApp.dir/mainMPI.cpp.o"
+  "pcmcApp.pdb"
+  "pcmcApp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pcmcApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ PCMCapp/test1/mainMPI.cpp
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <mpi.h>
+#include <functional>
+#include <string>
+
+#include "PCMC/PCAmacros.h"
+#include "PCMC/PolymerMC.h"
+#include "PCMC/PolymerObservable.h"
+#include "PCMC/Energy/Interaction.h"
+#include "PCMC/Energy/DoubleWell.h"
+#include "PCMC/Energy/LennardJones.h"
+#include "PCMC/MonteCarlo.h"
+
+using namespace std;
+using namespace PCA;
+
+
+int main(int np, char **p)
+{	
+    char* paramFileName;
+    int myCoreNumber, totalCoreNumber;
+    FILE* fp;
+    char* fname1;
+    
+    MPI_Init(&np, &p);
+    MPI_Comm_size(MPI_COMM_WORLD,&totalCoreNumber);
+    MPI_Comm_rank(MPI_COMM_WORLD,&myCoreNumber);
+    
+    fname1 = new char[100];
+    sprintf(fname1,"logs/core%imain.log",myCoreNumber);
+    fp = fopen(fname1, "w");
+    delete [] fname1;
+    _PCMC_WRITE_RUNTIME_CONTEXT(fp);
+
+
+    int len;
+    char ndName[1000];
+    MPI_Get_processor_name(ndName, &len);
+    std::string nodeName(ndName);
+    std::hash<std::string> hash_fn;
+    size_t nodeNameHash = hash_fn(nodeName);
+    fprintf(fp,"hashNodeName = %zu\n",nodeNameHash);
+    size_t seed = time(NULL)*(myCoreNumber+1)*nodeNameHash;
+    fprintf(fp,"Seed of randomGenerator: %zu\n", seed);
+    RandomGenerator::initialization(seed);
+    
+//RandomGenerator::initialization(1*(myCoreNumber+1));    
+    paramFileName = new char[100];
+    sprintf(paramFileName, "test.pcmc");
+    
+    Dictionary dictionary(paramFileName);
+    PolymerMC polymer(dictionary);
+    polymer.initWithRandomTaus();
+    
+    DoubleWell hamiltonian(dictionary);
+    LennardJones interaction(dictionary);
+   
+    MonteCarlo monteCarlo(dictionary);
+
+    monteCarlo.run(&polymer, &hamiltonian, &interaction, myCoreNumber, totalCoreNumber);
+    delete [] paramFileName;
+    
+    fprintf(fp,"Everything is ok!\n");
+    fclose(fp);
+MPI_Finalize();
+
+return 0;
+}
+    
